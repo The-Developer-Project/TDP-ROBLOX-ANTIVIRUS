a2f73c40e094a19d370a2a72165ce0ec3c35b3ee
Galactic Apricot <87578551+GalacticApricot@users.noreply.github.com>
2023-03-30 20:51:21 +0100

    Create TDPVirusScanner.lua

---
+++ TDPVirusScanner.lua
@@ -0,0 +1,21 @@
+local toolbar = plugin:CreateToolbar("TDPVirusScanner")
+
+local pluginButton = toolbar:CreateButton(
+	"Scan",
+	"Scans For Viruses",
+	"rbxassetid://12162246252")
+
+pluginButton.Click:Connect(function()
+	print('Scanning...')
+	local hits = 0
+	for _, v in ipairs(game:GetDescendants()) do
+		local s, e = pcall(function() if v:IsA('Script') or v:IsA('ModuleScript') or v:IsA('LocalScript') or v:IsA('BaseScript') then
+				if v.Source:match('MarketplaceService:PromptGamePassPurchase') then 
+					print('hit at: '..v:GetFullName())
+					hits += 1
+				end 
+			end 
+		end)
+	end
+	print('Finished Scan, Hits:'..hits)
+end)
